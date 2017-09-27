5ff12070fd152d277d8cdaf41fcea4972ba45471
John Marinelli <john.marinelli.dev@gmail.com>
2017-09-27 12:29:08 -0700

    Finish ep 18

--- src/configure-store.js
+++ src/configure-store.js
@@ -1,12 +1,12 @@
 import logger from 'redux-logger';
-import { createStore } from 'redux';
+import promise from 'redux-promise';
+import { createStore, applyMiddleware } from 'redux';
 import todo from './reducers';
 
 /*
  * receives store
  * returns a function that returns a function
  * (curried function)
- */
 const addPromiseSupportToDispatch = (store) => (next) => (action) => {
   if (typeof action.then === 'function') {
     return action.then(next);
@@ -14,17 +14,11 @@ const addPromiseSupportToDispatch = (store) => (next) => (action) => {
 
   return next(action);
 };
-
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-  middlewares
-    .reverse()
-    .forEach((mw) => store.dispatch = mw(store)(store.dispatch));
-};
+ */
 
 const configureStore = () => {
-  const store = createStore(todo);
-  const middlewares = [addPromiseSupportToDispatch, logger];
-  wrapDispatchWithMiddlewares(store, middlewares);
+  const middlewares = [promise, logger];
+  const store = createStore(todo, applyMiddleware(...middlewares));
   return store;
 };
 
